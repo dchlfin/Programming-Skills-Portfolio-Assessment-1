bc45ec0695bab1f20004bc95380695edf03adf7f
Chloe Gadingan - CC Level 5 <167969296+dchlfin@users.noreply.github.com>
2025-10-27 13:12:06 +0000

    Organized program into classes for better readability

--- 03-Student_Manager.py
+++ 03-Student_Manager.py
@@ -1,132 +1,114 @@
 import tkinter as tk
 from tkinter import ttk
 
-main = tk.Tk()
-main.title("Student Manager")
+class studentManager(tk.Tk):
+    def __init__(self):
 
-info = [
-    {"code": 9357, "name": "Grace Johns", "course_work": [10, 16, 4],"exam_score": 62},
-    {"code": 6727, "name": "Rosie Gray", "course_work": [12, 8, 16],"exam_score": 65},
-    {"code": 4175, "name": "Hallie Morrison", "course_work": [3, 15, 11],"exam_score": 64},
-    {"code": 8439, "name": "Jake Hobbs", "course_work": [10, 11, 10],"exam_score": 43},
-    {"code": 3921, "name": "Arthur Kelly", "course_work": [11, 7, 19],"exam_score": 93},
-    {"code": 7724, "name": "Freddie Scott", "course_work": [9, 11, 17],"exam_score": 63},
-    {"code": 9413, "name": "Caleb Hunter", "course_work": [7, 9, 20],"exam_score": 56},
-    {"code": 3746, "name": "George Peterson", "course_work": [14, 12, 13],"exam_score": 89},
-    {"code": 9643, "name": "Ivy Cameron", "course_work": [15, 19, 14],"exam_score": 96},
-    {"code": 6055, "name": "Arlo Martin", "course_work": [19, 12, 2],"exam_score": 55}
-]
+        # main setup
+        super().__init__()
+        self.title('Student Manager')
 
-record = None
-grade = None
+        # data handler
+        self.info = [
+        {'code': 9357, 'name': 'Grace Johns', 'course_work': [10, 16, 4],'exam_score': 62},
+        {'code': 6727, 'name': 'Rosie Gray', 'course_work': [12, 8, 16],'exam_score': 65},
+        {'code': 4175, 'name': 'Hallie Morrison', 'course_work': [3, 15, 11],'exam_score': 64},
+        {'code': 8439, 'name': 'Jake Hobbs', 'course_work': [10, 11, 10],'exam_score': 43},
+        {'code': 3921, 'name': 'Arthur Kelly', 'course_work': [11, 7, 19],'exam_score': 93},
+        {'code': 7724, 'name': 'Freddie Scott', 'course_work': [9, 11, 17],'exam_score': 63},
+        {'code': 9413, 'name': 'Caleb Hunter', 'course_work': [7, 9, 20],'exam_score': 56},
+        {'code': 3746, 'name': 'George Peterson', 'course_work': [14, 12, 13],'exam_score': 89},
+        {'code': 9643, 'name': 'Ivy Cameron', 'course_work': [15, 19, 14],'exam_score': 96},
+        {'code': 6055, 'name': 'Arlo Martin', 'course_work': [19, 12, 2],'exam_score': 55}
+        ]   
 
-# individual record overrides the viewAll
+        # search record
+        self.clicked = tk.StringVar()
 
-def viewAll():
-    global record
-    global grade 
-    for student in info:
-        cw, es = student["course_work"], student["exam_score"]
-        cw_total = sum(cw)
-        # pct = round(((cw_total + es) / 160) * 100)
-        pct = round((cw_total + es) / 160 * 100, 2)
-        
-        if pct < 40:
-            grade = "F"
-        elif pct >= 40 and pct < 50:
-            grade = "D"
-        elif pct >= 50 and pct < 60:
-            grade = "C"
-        elif pct >= 60 and pct < 70:
-            grade = "B"
-        elif pct >= 70:
-            grade = "A"
 
-        details = f"Name: {student["name"]}\nNumber: {student["code"]}\nCoursework Total: {cw_total}\nExam Mark: {es}\nOverall Percentage: {pct}%\nGrade: {grade}\n"
+        # widgets 
+        self.menu = Menu(self)
+        self.menu.grid(row = 0, column = 0, padx = 50, pady = 10, sticky = 'EW')
 
-        if record:
-                record.grid_forget()
+        self.records = Records(self, self.info, self.clicked)
+        self.records.grid(row = 1, column = 0, padx = 50, pady = 10, sticky = 'EW')
 
-        for r in range(10):
-            record = tk.Label(f3, text = details, justify = "left", background = "white")
-            record.grid(row = r, sticky = "W", pady = (0, 5))
-      
-        
+        self.record_display = RecordsDisplay(self)
+        self.record_display.grid(row = 3, column = 0, columnspan = 4, padx = 50, pady = 10, sticky = 'NSEW')
+        self.record_display.grid_propagate(False)
 
-def selected():
-    global record
-    global grade
-    name = clicked.get()
-    for student in info:
-        if student["name"] == name:
-            cw, es = student.get("course_work"), student.get("exam_score")
-            cw_total = sum(cw)
-            # pct = round(((cw_total + es) / 160) * 100)
-            pct = round((cw_total + es) / 160 * 100, 2)
+        # run
+        self.mainloop()
+
+    # def formatted_info(self):
+    #     format = []
+
+    #     for student in self.info:
+    #         cw, es = student.get('course_work'), student.get('exam_score')
+    #         cw_total = sum(cw)
+    #         pct = round((cw_total + es) / 160 * 100, 2)
+
+    #         if pct < 40:
+    #             grade = 'F'
+    #         elif pct >= 40 and pct < 50:
+    #             grade = 'D'
+    #         elif pct >= 50 and pct < 60:
+    #             grade = 'C'
+    #         elif pct >= 60 and pct < 70:
+    #             grade = 'B'
+    #         elif pct >= 70:
+    #             grade = 'A'
             
-            if pct < 40:
-                grade = "F"
-            elif pct >= 40 and pct < 50:
-                grade = "D"
-            elif pct >= 50 and pct < 60:
-                grade = "C"
-            elif pct >= 60 and pct < 70:
-                grade = "B"
-            elif pct >= 70:
-                grade = "A"
+    #         format = f'Name: {student.get('name')}\nNumber: {student.get('code')}\nCoursework Total: {cw_total}\nExam Mark: {es}\nOverall Percentage: {pct}%\nGrade: {grade}'
 
-            details = f"Name: {student.get("name")}\nNumber: {student.get("code")}\nCoursework Total: {cw_total}\nExam Mark: {es}\nOverall Percentage: {pct}%\nGrade: {grade}"
+    #         if record: 
+    #             record.grid_forget()
+            
+            
 
-            if record:
-                record.grid_forget()
+class Menu(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        ttk.Label(self, text = 'Student Manager', font = ('Calibri', 15)).grid(row = 0, column = 0, columnspan = 3, pady = (0, 20))
 
-            record = tk.Label(f3, text = details, justify = "left", background = "white")
-            record.grid(row = 0, sticky = "W", pady = (0, 5))
-            break
+        self.buttons()
+
+    def buttons(self):
+        records = ttk.Button(self, text = 'View All Student Records')
+        records.grid(row = 1, column = 0, padx = (0,15), ipadx = 5, ipady = 5, sticky = 'W')
+        high = ttk.Button(self, text = 'Show Highest Score')
+        high.grid(row = 1, column = 1, ipadx = 5, ipady = 5, sticky = tk.W)
+        low = ttk.Button(self, text = 'Show Lowest Score')
+        low.grid(row = 1, column = 2, ipadx = 5, ipady = 5, padx = (15,0), sticky = tk.W)
+
+class Records(ttk.Frame):
+    def __init__(self, parent, info, clicked): 
+        super().__init__(parent)
+        self.info = info
+        self.clicked = clicked
+        self.widgets()
+        
+    def widgets(self):
+        names = [student['name'] for student in self.info]
+        ttk.Label(self, text = 'View Individual Student Record:', font = ('Arial', 11)).grid(row = 0, column = 0,  ipadx = 5, sticky = 'W')
 
 
-clicked = tk.StringVar()
+        search_record = ttk.Combobox(self, width = 15, textvariable = self.clicked, values = names)
+        search_record.grid(row = 0, column = 1, sticky = 'EW')
+        view_record = ttk.Button(self, text = 'View Record')
+        view_record.grid(row = 0, column = 3, padx = (10,0), ipadx = 5, ipady = 5)
 
-names = [student["name"] for student in info]
+class RecordsDisplay(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent, width = 400, height = 125, borderwidth = 1, relief = 'groove')
+        self.configure(style = 'Display.TFrame')
 
-# first frame
-f1 = tk.Frame(main, width = 500, height = 100)
-f1.grid(row = 0, column = 0, padx = 50, pady = 10, sticky = "EW", )
+        s = ttk.Style()
+        s.configure('Display.TFrame', background = 'white')
 
-l1 = ttk.Label(f1, text = "Student Manager", font = (11)).grid(row = 0, column = 0, columnspan = 3, pady = (0,20))
 
-# buttons
-records = ttk.Button(f1, text = "View All Student Records", command = viewAll).grid(row = 1, column = 0, padx = (0,15), ipadx = 5, ipady = 5, sticky = "W")
-high = ttk.Button(f1, text = "Show Highest Score").grid(row = 1, column = 1, ipadx = 5, ipady = 5, sticky = tk.W,)
-low = ttk.Button(f1, text = "Show Lowest Score").grid(row = 1, column = 2, ipadx = 5, ipady = 5, padx = (15,0), sticky = tk.W,)
+def main():
+    studentManager()
 
-# second frame
-f2 = tk.Frame(main, width = 500, height = 200)
-f2.grid(row = 1, column = 0, padx = 50, pady = 10, sticky = "EW")
-l2 = ttk.Label(f2, text = "View Individual Student Record:", font = ('Arial', 11)).grid(row = 0, column = 0,  ipadx = 5, sticky = "W")
-
-# combobox
-names_combo = ttk.Combobox(f2, width = 15, textvariable = clicked, values = names)
-names_combo.grid(row = 0, column = 1, sticky = "EW")
-
-view_record = ttk.Button(f2, text = "View Record", command = selected).grid(row = 0, column = 3, padx = (10, 0), ipadx = 5, ipady = 5)
-
-# listbox = ttk.Listbox(f2, height = 7, listvariable="")
-f3 = ttk.Frame(main, width = 400, height = 125, borderwidth = 1, relief = "groove")
-# sb = tk.Scrollbar(f3, orient = "vertical")
-# sb.grid(row = 0, column = 1, sticky = "NS")
-f3.grid(row = 2, column = 0, columnspan = 4, padx = 50, pady = 10, sticky = "NSEW")
-f3.grid_propagate(False)
-
-# sb = tk.Scrollbar(f3, orient = "vertical")
-# sb.grid(sticky = "E")
-# cv = tk.Canvas(f3, bd = 0, highlightthickness = 0, yscrollcommand = sb.set)
-# cv.grid(row = 0, column = 0, sticky = "E")
-
-# cv.xview_moveto(0)
-# cv.yview_moveto(0)
-
-s = ttk.Style()
-s.configure('TFrame', background = 'white')
-
-main.mainloop()
+if __name__ == '__main__':
+    main()
