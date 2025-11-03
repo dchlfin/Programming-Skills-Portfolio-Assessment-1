a44add2f29c8e509fedfc2c15376d530bbee77f4
Chloe Gadingan - CC Level 5 <167969296+dchlfin@users.noreply.github.com>
2025-11-03 17:04:23 +0000

    add basic calculation functionality for two integers

---
+++ CodeLab II Chapters/exercise_2_maths.py
@@ -0,0 +1,7 @@
+n1 = int(input("Enter your first number: "))
+n2 = int(input("Enter your second number: "))
+
+def maths():
+    print(f"""\nResults by Operation: Sum (+): {n1 + n2} | Difference (-): {n1 - n2} | Product (x): {n1 * n2} | Quotient (/): {n1 / n2} | Remainder (%): {n1 % n2}""")
+
+maths()
