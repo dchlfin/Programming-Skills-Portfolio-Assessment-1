3f4778fcc09df40ef32b173a339006800b6738b5
Chloe Gadingan - CC Level 5 <167969296+dchlfin@users.noreply.github.com>
2025-11-03 17:21:53 +0000

    add pattern printing functionality using nested loops

---
+++ CodeLab II Chapters/exercise_8_print_pattern.py
@@ -0,0 +1,11 @@
+def pat(n):
+    num = 1
+    for i in range(0, n):
+        num = 1
+        for j in range(0, i+1):
+            print(num, end = " ")
+            num = num + 1
+        print("")
+
+n = 5
+pat(n)
