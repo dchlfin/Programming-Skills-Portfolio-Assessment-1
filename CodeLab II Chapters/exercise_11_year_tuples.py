21e49caeb00302b46cb8667206a3cb84a2628d1e
Chloe Gadingan - CC Level 5 <167969296+dchlfin@users.noreply.github.com>
2025-11-03 17:31:42 +0000

    add a program of different tuple methods on a tuple

---
+++ CodeLab II Chapters/exercise_11_year_tuples.py
@@ -0,0 +1,13 @@
+year = (2017, 2003, 2011, 2005, 1987, 2009, 2020, 2018, 2009)
+
+third_to_last = year[-3]
+reversed_years = year[::-1]
+count_2009 = year.count(2009)
+id_2018 = year.index(2018)
+year_len = len(year)
+
+print(third_to_last)
+print(reversed_years)
+print(count_2009)
+print(id_2018)
+print(year_len)
