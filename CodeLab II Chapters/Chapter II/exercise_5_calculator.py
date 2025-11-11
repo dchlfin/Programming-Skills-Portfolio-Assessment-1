1a5fbc8c7be2ba6d995011b0d6f1c0065379e060
Chloe Gadingan - CC Level 5 <167969296+dchlfin@users.noreply.github.com>
2025-11-11 21:44:58 +0000

    docs: comment widgets by row

--- CodeLab II Chapters/Chapter II/exercise_5_calculator.py
+++ CodeLab II Chapters/Chapter II/exercise_5_calculator.py
@@ -45,19 +45,26 @@ class Numbers(ttk.Frame):
 
         # widgets
         entry = ttk.Entry(self, style = 'calc.TEntry', **entry_font).grid(row = 0, column = 0, columnspan = 4, padx = 5, pady = (0, 5), sticky = EW)
-        
+
+        # row 1
         ttk.Button(self, text = "÷").grid(row = 1, column = 3)
         ttk.Button(self, text = "9", command = lambda: enter(9)).grid(row = 1, column = 2)
         ttk.Button(self, text = "8", command = lambda: enter(8)).grid(row = 1, column = 1)
         ttk.Button(self, text = "7", command = lambda: enter(7)).grid(row = 1, column = 0)
+        
+        # row 2
         ttk.Button(self, text = "×").grid(row = 2, column = 3)
         ttk.Button(self, text = "6", command = lambda: enter(6)).grid(row = 2, column = 2)
         ttk.Button(self, text = "5", command = lambda: enter(5)).grid(row = 2, column = 1)
         ttk.Button(self, text = "4", command = lambda: enter(4)).grid(row = 2, column = 0)
+        
+        # row 3
         ttk.Button(self, text = "-").grid(row = 3, column = 3)
         ttk.Button(self, text = "3", command = lambda: enter(3)).grid(row = 3, column = 2)
         ttk.Button(self, text = "2", command = lambda: enter(2)).grid(row = 3, column = 1)
         ttk.Button(self, text = "1", command = lambda: enter(1)).grid(row = 3, column = 0)
+        
+        # row 4
         ttk.Button(self, text = "+").grid(row = 4, column = 3)
         ttk.Button(self, text = "=").grid(row = 4, column = 2)
         ttk.Button(self, text = "0", command = lambda: enter(0)).grid(row = 4, column = 1, sticky = EW)
