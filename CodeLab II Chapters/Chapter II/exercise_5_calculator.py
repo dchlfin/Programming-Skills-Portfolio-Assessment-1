35272ff2f399fe131525d9685fadd9c2a8edd35a
Chloe Gadingan - CC Level 5 <167969296+dchlfin@users.noreply.github.com>
2025-11-11 16:43:51 +0000

    feat: create calculator GUI

---
+++ CodeLab II Chapters/Chapter II/exercise_5_calculator.py
@@ -0,0 +1,69 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+class Calculator(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Basic Calculator")
+        self.propagate(False)
+
+        # styles
+        self.entry_font = {'font': ('Helvetica', 15)}
+        self.s = ttk.Style()
+        self.s.configure('calc.TButton', font = ('Helvetica', 15))
+
+        self.columnconfigure(0, weight = 1)
+        self.columnconfigure(1, weight = 1)
+        self.columnconfigure(2, weight = 1)
+
+        # widgets
+        self.entry = Entry(self, self.entry_font)
+        self.entry.grid(row = 0, column = 0, columnspan = 3, padx = 5, pady = 5, sticky = EW)
+        self.numbers = Numbers(self, self.s)
+        self.numbers.grid(row = 1, column = 0, columnspan = 3, padx = 5, pady = (0, 5), sticky = EW)
+    
+        # calculation methods
+
+        # call
+        self.mainloop()
+
+class Entry(ttk.Frame):
+    def __init__(self, parent, entry_font):
+        super().__init__(parent)
+        self.entry_font = entry_font
+        self.columnconfigure(0, weight = 1)
+        
+        ttk.Entry(self, style = 'calc.TEntry', **entry_font).grid(row = 0, column = 0, sticky = EW)
+
+class Numbers(ttk.Frame):
+    def __init__(self, parent, entry_font):
+        super().__init__(parent)
+        self.entry_font = entry_font
+
+        # columns
+        self.columnconfigure(0, weight = 1)
+        self.columnconfigure(1, weight = 1)
+        self.columnconfigure(2, weight = 1)
+        self.columnconfigure(3, weight = 1)
+
+        # widgets
+        ttk.Button(self, text = "÷").grid(row = 0, column = 3)
+        ttk.Button(self, text = "9").grid(row = 0, column = 2)
+        ttk.Button(self, text = "8").grid(row = 0, column = 1)
+        ttk.Button(self, text = "7").grid(row = 0, column = 0)
+        ttk.Button(self, text = "x").grid(row = 1, column = 3)
+        ttk.Button(self, text = "6").grid(row = 1, column = 2)
+        ttk.Button(self, text = "5").grid(row = 1, column = 1)
+        ttk.Button(self, text = "4").grid(row = 1, column = 0)
+        ttk.Button(self, text = "-").grid(row = 2, column = 3)
+        ttk.Button(self, text = "3").grid(row = 2, column = 2)
+        ttk.Button(self, text = "2").grid(row = 2, column = 1)
+        ttk.Button(self, text = "1").grid(row = 2, column = 0)
+        ttk.Button(self, text = "+").grid(row = 3, column = 3)
+        ttk.Button(self, text = "=").grid(row = 3, column = 2)
+        ttk.Button(self, text = "0").grid(row = 3, column = 0, columnspan = 2, sticky = EW)
+
+if __name__ == "__main__":
+    Calculator()
+
