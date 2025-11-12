51491fbc2d2eab779b55edc925041296798a060a
Chloe Gadingan - CC Level 5 <167969296+dchlfin@users.noreply.github.com>
2025-11-12 21:29:00 +0000

    feat: refactor input frame and add greeting function
    initialize input frame widgets 'name' and 'color' entries to instance variables, and add greeting functionality

--- CodeLab II Chapters/Chapter III/exercise_1_greeting_app.py
+++ CodeLab II Chapters/Chapter III/exercise_1_greeting_app.py
@@ -20,9 +20,9 @@ class Greeting(Tk):
         # run
         self.mainloop()
 
-class InputFrame(ttk.Frame):
+class InputFrame(Frame):
     def __init__(self, parent):
-        super().__init__(parent) 
+        super().__init__(parent, height = 100, highlightthickness = 1, highlightbackground = 'gray')
         
         # layout configuration
         # columns
@@ -37,22 +37,47 @@ class InputFrame(ttk.Frame):
 
         # name
         Label(self, text = "Enter your Name:", background = 'white').grid(row = 1, column = 0, ipadx = 5, pady = (10, 5), sticky = E)
-        name = ttk.Entry(self)
-        name.grid(row = 1, column = 1, padx = 5, pady = (0, 5), sticky = EW)
+        self.name = ttk.Entry(self)
+        self.name.grid(row = 1, column = 1, padx = 5, pady = (0, 5), sticky = EW)
 
         # color
         Label(self, text = "Select a Color:", background = 'white').grid(row = 2, column = 0, padx = 5, pady = (0, 5),sticky = E)
 
-        colors = ['red', 'orange', 'yellow', 'green', 'blue', 'violet']
-        color = ttk.Combobox(self, values = colors)
-        color.grid(row = 2, column = 1, padx = 5, pady = (0, 5), sticky = EW)
+        # combobox
+        self.colors_info = [
+            {'name': 'red', 'hex_bg': '#FF9E97', 'hex_fg': '#DC3E4D'},
+            {'name': 'orange', 'hex_bg': '#FFDBA7', 'hex_fg': '#E8A25C'},
+            {'name': 'yellow', 'hex_bg': '#FFF6BF', 'hex_fg': '#F4CC0A'},
+            {'name': 'green', 'hex_bg': '#E9FFDB', 'hex_fg': '#80CA9F'},
+            {'name': 'blue', 'hex_bg': '#CBECFF', 'hex_fg': '#03B1EE'},
+            {'name': 'purple', 'hex_bg': '#EBCFFF', 'hex_fg': '#7544B4'}
+        ]
+
+        colors = [color['name'] for color in self.colors_info]
+
+        self.color = ttk.Combobox(self, values = colors)
+        self.color.grid(row = 2, column = 1, padx = 5, pady = (0, 5), sticky = EW)
 
         # update greeting
-        Button(self, text = "Update Greeting", font = ('Helvetica', 9,  'bold'), background = '#03B1EE', foreground = '#CBECFF', relief = FLAT).grid(row = 4, column = 0, columnspan = 3, pady = 10, ipadx = 5, ipady = 5)
+        Button(self, text = "Update Greeting", font = ('Helvetica', 9,  'bold'), background = '#03B1EE', foreground = '#CBECFF', relief = FLAT, command = self.update_greeting).grid(row = 4, column = 0, columnspan = 3, pady = 10, ipadx = 5, ipady = 5)
 
         # call
         self.display_frame = DisplayFrame(self)
         self.display_frame.grid(row = 3, column = 0, columnspan = 3, padx = 5, pady = (10, 0), sticky = EW)
+
+    def update_greeting(self):
+        n = self.name.get().strip()
+        c = self.color.get()
+
+        if not n: 
+            n = "User"
+
+        for color in self.colors_info:
+            if color['name'] == c:
+                self.display_frame.display.config(text = f"Hello, {n.capitalize()}!", background = color['hex_bg'], foreground = color['hex_fg'])
+                break
+        else:
+            self.display_frame.display.config(text = f"Hello {n.capitalize()}!", background = '#f0f0f0', foreground = 'black')
 
 class DisplayFrame(Frame):
     def __init__(self, parent):
